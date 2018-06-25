cd7eeac1be05594ee3c822a5278d4a44b38fa870
amvui <you@example.com>
2018-06-25 20:10:55 +0700

    25 juni 2018

---
+++ src/whatever/src/image/image_front.cpp
@@ -0,0 +1,30 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+using namespace cv;
+
+Mat image;
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "videoFront");
+	ros::NodeHandle nh;
+	image_transport::ImageTransport it(nh);
+	image_transport::Publisher pub = it.advertise("/kkctbn/image/camera", 1, true);
+
+	VideoCapture cap(0); 
+	if(!cap.isOpened()){
+		ROS_ERROR ("Error opening camera.");	  
+		return 1;
+	}
+	while (nh.ok()) {
+		cap.read(image);
+		if(!image.empty()){	
+			sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+			pub.publish(msg);
+		}
+		ros::spinOnce();
+	}
+}
